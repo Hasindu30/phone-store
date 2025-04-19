cfdad122fe073c4f06d5de518568308f5fd83a69
Hasindu <chandeepahasindu@gmail.com>
2025-04-19 23:22:52 +0530

    home pager update the line

--- src/mockData/data.js
+++ src/mockData/data.js
@@ -26,7 +26,7 @@ export const NavbarMenu = [
     },
     
 
-]
+];
 
 export const NewArrivalProducts = [
     {
@@ -75,3 +75,75 @@ export const NewArrivalProducts = [
       stockType: 'in',
     },
   ];
+
+  export const BackProducts = [
+    {
+      id: 1,
+      name: 'OnePlus Nord Buds 3 Pro Wireless Earbuds',
+      price: 'Rs 15,999.00',
+      oldPrice: 'Rs 18,999.00',
+      installment: 'Rs 5,333.00',
+      stock: 'In stock',
+      stockType: 'in',
+      discount: 16,
+      rating: 4.6,
+      colors: 2,
+      badge: 'Top Pick',
+      image: 'oneplus.jpg',
+    },
+    {
+      id: 2,
+      name: 'Insta360 GO 3S 4K Camera (Standard Bundle)',
+      price: 'Rs 139,999.00',
+      oldPrice: 'Rs 149,999.00',
+      installment: 'Rs 46,666.33',
+      stock: '1 in stock',
+      stockType: 'low',
+      discount: 7,
+      rating: 4.9,
+      colors: 2,
+      image: 'insta360.jpg',
+    },
+    {
+      id: 3,
+      name: 'Xiaomi Mi Robot Vacuum S20+ (S20 Plus)',
+      price: 'Rs 89,999.00',
+      oldPrice: 'Rs 94,999.00',
+      installment: 'Rs 29,999.66',
+      stock: 'In stock',
+      stockType: 'in',
+      discount: 5,
+      rating: 4.7,
+      colors: 3,
+      image: 'vacuum.jpg',
+    },
+    {
+      id: 4,
+      name: 'Anker Soundcore Rave 3 Wireless Karaoke Party Speaker',
+      price: 'Rs 89,999.00',
+      oldPrice: 'Rs 99,999.00',
+      installment: 'Rs 29,999.66',
+      stock: 'In stock',
+      stockType: 'in',
+      discount: 10,
+      rating: 4.8,
+      colors: 3,
+      badge: 'Best Seller',
+      image: 'speaker.jpg',
+    },
+    {
+      id: 5,
+      name: 'Anker Soundcore Rave 3 Wireless Karaoke Party Speaker',
+      price: 'Rs 89,999.00',
+      oldPrice: 'Rs 99,999.00',
+      installment: 'Rs 29,999.66',
+      stock: 'In stock',
+      stockType: 'in',
+      discount: 10,
+      rating: 4.8,
+      colors: 3,
+      badge: 'Best Seller',
+      image: 'speaker.jpg',
+    },
+  ];
+  
